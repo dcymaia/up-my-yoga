c3e0a0e2ad92dcdc6fbbe6c444ec9ddbaf5b93a5
Danilo Maia <dcymaia@gmail.com>
2020-10-21 01:25:56 -0300

    Go

--- config/make_links.sh
+++ config/make_links.sh
@@ -5,6 +5,8 @@ CONFDIR=$HOME/up-yoga/config
 
 
 ln -s $CONFDIR/.vimrc $HOME
+ln -s $CONFDIR/.bashrc $HOME
+ln -s $CONFDIR/.xinitrc $HOME
 
 mkdir -p $HOME/.config/i3
 ln -s $CONFDIR/i3 $HOME/.config/i3/config
@@ -14,3 +16,5 @@ ln -s $CONFDIR/i3status $HOME/.config/i3status/config
 
 ln -s $CONFDIR/.Xresources $HOME
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
