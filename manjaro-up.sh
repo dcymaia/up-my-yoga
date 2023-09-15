1a6546ac795dd99677487ffcb97c50725b3c6db1
Danilo Maia <dcymaia@gmail.com>
2023-09-15 11:18:55 -0300

    go

--- manjaro-up.sh
+++ manjaro-up.sh
@@ -2,16 +2,34 @@
 #
 #	
 
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 sudo pacmac -S \
     discord \
     telegram-desktop \
     steam \
+    docker \
+    docker-compose \
+    the_silver_searcher \
+
 
 sudo pamac install
 
 sudo pamac build \
     google-chrome \
     vim-plug \
+    postman \
+    google-earth \
+
+
+# Docker
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
+sudo usermod -aG docker $USER
+
+# Dark theme for Gnome Terminal
+git clone https://github.com/dracula/gnome-terminal
+cd gnome-terminal
+./install.sh
+
+
